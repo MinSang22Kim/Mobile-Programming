0ad5174ef05928ec86e8057a4ecf723d8d1d6899
MinSang22Kim <powerminsang9901@gmail.com>
2024-09-20 14:35:41 +0900

    모바일 프로그래밍 2주차 실습코드

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.firstapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+    public void onClickButton(View v) {
+        // 토스트는 동자겡 대한 간단한 피드백을 작은 팝업으로 표시함
+        Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show();
+    }
+}
