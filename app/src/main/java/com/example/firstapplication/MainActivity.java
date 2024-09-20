4027b4f68ec7172a194abed51466a4c8a838eaf9
MinSang22Kim <powerminsang9901@gmail.com>
2024-09-20 16:55:21 +0900

    모바일 프로그래밍 3주차 실습코드

--- app/src/main/java/com/example/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -2,26 +2,43 @@ package com.example.firstapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Android 액티비티 생명주기에서 가장 먼저 호출되어 초기화 해주는 메서드
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // 액티비티 기본 설정 및 상태 복원 작업 처리
 //        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // xml 레이아웃 파일을 액티비티에 연결하여 화면 구성 정의
+
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+
+        Button b = (Button) findViewById((R.id.button3));
+        // 자바로 이벤트 리스너 구현
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) findViewById(R.id.myText);
+                textView.setText("Change!!!!!!");
+            }
+        });
     }
 
+    // 버튼을 클릭하면 토스트(팝업)와 함께 텍스트를 대치해주는 메서드, xml 지정 방식
     public void onClickButton(View v) {
-        // 토스트는 동자겡 대한 간단한 피드백을 작은 팝업으로 표시함
+        // 토스트: 작은 팝업을 표시하여 동작에 대한 피드백 표시
         Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show();
+        TextView textView = (TextView) findViewById(R.id.myText); // id로 객체 찾아, 형변환 & 저장
+        textView.setText("Change!"); // 텍스트 속성 세팅
+
     }
 }
