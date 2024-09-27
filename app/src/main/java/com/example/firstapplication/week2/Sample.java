58d88bce5fbad8851e260f52aa1f4abeeea3ab6b
MinSang22Kim <powerminsang9901@gmail.com>
2024-09-27 16:54:01 +0900

    "모바일 프로그래밍 2주차 실습코드"

---
+++ app/src/main/java/com/example/firstapplication/week2/Sample.java
@@ -0,0 +1,17 @@
+package com.example.firstapplication.week2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.firstapplication.R;
+
+public class Sample extends AppCompatActivity {
+    // Android 액티비티 생명주기에서 가장 먼저 호출되어 초기화 해주는 메서드
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // 액티비티 기본 설정
+        setContentView(R.layout.activity_main_week2); // 사용할 레이아웃 설정
+    }
+}
