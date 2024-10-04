ee98571489d36d6b2ec92908e35b2c04c3a614af
MinSang22Kim <powerminsang9901@gmail.com>
2024-10-04 16:20:58 +0900

    모바일 프로그래밍 5주차 실습코드

--- app/src/main/java/com/example/firstapplication/week5/ImagePrac.java
+++ app/src/main/java/com/example/firstapplication/week5/ImagePrac.java
@@ -2,8 +2,6 @@ package com.example.firstapplication.week5;
 
 import android.os.Bundle;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,28 +11,13 @@ public class ImagePrac extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_week5_radio_image);
-
-        RadioButton r1 = (RadioButton) findViewById(R.id.radioButton1);
-        RadioButton r2 = (RadioButton) findViewById(R.id.radioButton2);
+        setContentView(R.layout.activity_main_week5_layoutprac);
 
         CompoundButton.OnCheckedChangeListener l = new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                ImageView imageView = (ImageView) findViewById(R.id.imageButton);
 
-                if (compoundButton.getId() == R.id.radioButton1 && b) {
-                    imageView.setImageResource(R.drawable.sample1);
-                } else if (compoundButton.getId() == R.id.radioButton2 && b) {
-                    imageView.setImageResource(R.drawable.sample2);
-                }
             }
         };
-
-        r1.setOnCheckedChangeListener(l);
-        r2.setOnCheckedChangeListener(l);
-
-//        ImageView i = (ImageView) findViewById(R.id.imageButton);
-//        i.setImageResource(R.drawable.sample2);
     }
 }
